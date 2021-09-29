f99521b4e9bf80c076dddde1b960f5fac08d78df
Enchanter_Xiao <991784084@qq.com>
2021-09-29 10:27:27 +0800

    'eval'

--- eval_voc.sh
+++ eval_voc.sh
@@ -1,1 +1,4 @@
-python eval.py --resume ../1sw/run/pascal/deeplab-resnet/model_best.pth.tar --save_dir ../1sw/output/baseline/ --backbone resnet --batch-size 1 --gpu-ids 0 --dataset pascal --mode eval
+
+resume=../1sw/run/pascal/deeplab-resnet/model_best.pth.tar
+save_dir=../1sw/output/baseline/
+python eval.py --resume $resume --save_dir $save_dir --backbone resnet --batch-size 1 --gpu-ids 0 --dataset pascal --mode eval
