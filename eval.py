13460436b4c3415b2198683016ab5dd17e253f01
Enchanter_Xiao <991784084@qq.com>
2021-09-29 10:25:55 +0800

    'eval'

--- eval.py
+++ eval.py
@@ -71,6 +71,14 @@ class Evaler(object):
     def __init__(self, args, palette):
         self.args = args
         self.palette = palette
+        self.root_path = args.save_dir
+        if not os.path.exists(self.root_path):
+            os.mkdir(self.root_path)
+        if not os.path.exists(os.path.join(self.root_path, 'vis')):
+            os.mkdir(os.path.join(self.root_path, 'vis'))
+        if not os.path.exists(os.path.join(self.root_path, 'mask')):
+            os.mkdir(os.path.join(self.root_path, 'mask'))
+
         # Define Dataloader
         kwargs = {'num_workers': args.workers, 'pin_memory': True}
         self.train_loader, self.val_loader, self.test_loader, self.nclass = make_data_loader(args, **kwargs)
@@ -143,8 +151,6 @@ def main():
     parser.add_argument('--split', type=str, default='train_augvoc',
                         choices=['train_augvoc', 'val_voc', 'train_gen_bsl'],
                         help='dataset name (default: pascal)')
-    parser.add_argument('--use-sbd', action='store_true', default=True,
-                        help='whether to use SBD dataset (default: True)')
     parser.add_argument('--workers', type=int, default=4,
                         metavar='N', help='dataloader threads')
     parser.add_argument('--base-size', type=int, default=513,
@@ -155,34 +161,11 @@ def main():
                         help='whether to use sync bn (default: auto)')
     parser.add_argument('--freeze-bn', type=bool, default=False,
                         help='whether to freeze bn parameters (default: False)')
-    parser.add_argument('--loss-type', type=str, default='ce',
-                        choices=['ce', 'focal'],
-                        help='loss func type (default: ce)')
     # training hyper params
-    parser.add_argument('--epochs', type=int, default=None, metavar='N',
-                        help='number of epochs to train (default: auto)')
-    parser.add_argument('--start_epoch', type=int, default=0,
-                        metavar='N', help='start epochs (default:0)')
     parser.add_argument('--batch-size', type=int, default=None,
                         metavar='N', help='input batch size for \
                                 training (default: auto)')
-    parser.add_argument('--test-batch-size', type=int, default=None,
-                        metavar='N', help='input batch size for \
-                                testing (default: auto)')
-    parser.add_argument('--use-balanced-weights', action='store_true', default=False,
-                        help='whether to use balanced weights (default: False)')
-    # optimizer params
-    parser.add_argument('--lr', type=float, default=None, metavar='LR',
-                        help='learning rate (default: auto)')
-    parser.add_argument('--lr-scheduler', type=str, default='poly',
-                        choices=['poly', 'step', 'cos'],
-                        help='lr scheduler mode: (default: poly)')
-    parser.add_argument('--momentum', type=float, default=0.9,
-                        metavar='M', help='momentum (default: 0.9)')
-    parser.add_argument('--weight-decay', type=float, default=5e-4,
-                        metavar='M', help='w-decay (default: 5e-4)')
-    parser.add_argument('--nesterov', action='store_true', default=False,
-                        help='whether use nesterov (default: False)')
+
     # cuda, seed and logging
     parser.add_argument('--no-cuda', action='store_true', default=
                         False, help='disables CUDA training')
@@ -194,19 +177,11 @@ def main():
     # checking point
     parser.add_argument('--resume', type=str, default=None,
                         help='put the path to resuming file if needed')
-    parser.add_argument('--checkname', type=str, default=None,
-                        help='set the checkpoint name')
-    # finetuning pre-trained models
-    parser.add_argument('--ft', action='store_true', default=False,
-                        help='finetuning on a different dataset')
-    # evaluation option
-    parser.add_argument('--eval-interval', type=int, default=1,
-                        help='evaluuation interval (default: 1)')
-    parser.add_argument('--no-val', action='store_true', default=False,
-                        help='skip validation during training')
+    parser.add_argument('--save_dir', type=str, default=None,
+                        help='put the path to resuming file if needed')
 
     args = parser.parse_args()
-    args.split = 'train_augvoc'
+    args.split = 'val_voc'
     args.cuda = not args.no_cuda and torch.cuda.is_available()
     if args.cuda:
         try:
@@ -220,33 +195,6 @@ def main():
         else:
             args.sync_bn = False
 
-    # default settings for epochs, batch_size and lr
-    if args.epochs is None:
-        epoches = {
-            'coco': 30,
-            'cityscapes': 200,
-            'pascal': 50,
-        }
-        args.epochs = epoches[args.dataset.lower()]
-
-    if args.batch_size is None:
-        args.batch_size = 4 * len(args.gpu_ids)
-
-    if args.test_batch_size is None:
-        args.test_batch_size = args.batch_size
-
-    if args.lr is None:
-        lrs = {
-            'coco': 0.1,
-            'cityscapes': 0.01,
-            'pascal': 0.007,
-        }
-        args.lr = lrs[args.dataset.lower()] / (4 * len(args.gpu_ids)) * args.batch_size
-
-
-    if args.checkname is None:
-        args.checkname = 'deeplab-'+str(args.backbone)
-
     print(args)
     cmap = colormap()
     palette = ImagePalette.ImagePalette()
@@ -254,14 +202,7 @@ def main():
         palette.getcolor(rgb)
     torch.manual_seed(args.seed)
     trainer = Evaler(args, palette)
-    print('Starting Epoch:', trainer.args.start_epoch)
-    print('Total Epoches:', trainer.args.epochs)
-    for epoch in range(trainer.args.start_epoch, trainer.args.epochs):
-        trainer.training(epoch)
-        if not trainer.args.no_val and epoch % args.eval_interval == (args.eval_interval - 1):
-            trainer.validation(epoch)
-
-    trainer.writer.close()
+    trainer.validation()
 
 if __name__ == "__main__":
    main()
--- eval_voc.sh
+++ eval_voc.sh
@@ -1,1 +1,1 @@
-python eval.py --backbone resnet --batch-size 1 --gpu-ids 0 --resume deeplab-resnet --dataset pascal --mode eval
+python eval.py --resume ../1sw/run/pascal/deeplab-resnet/model_best.pth.tar --save_dir ../1sw/output/baseline/ --backbone resnet --batch-size 1 --gpu-ids 0 --dataset pascal --mode eval
