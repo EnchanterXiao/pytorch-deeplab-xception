51093e12ecc342c9c4023e0ea0df6039c0060848
Enchanter_Xiao <991784084@qq.com>
2021-09-30 09:50:47 +0800

    'gen_mask_path'

---
+++ Generate_maskPath.py
@@ -0,0 +1,18 @@
+import os
+'''
+生成伪语义信息的路径文件
+'''
+
+read_file = '../1sw/data/train_augvoc.txt'
+save_file = '../1sw/data/train_gen_bsl.txt'
+mask_path = '/home/lwq/sdb1/xiaoxin/WSSS/1sw/output/pascal_voc/v0920/bsl/train_augvoc_1/no_crf'
+result = []
+img_gt_name_list = open(read_file).read().splitlines()
+with open(save_file, 'w') as f:
+    for path in img_gt_name_list:
+        path = path.split(" ")[0]
+        f.write(path)
+        filename = path.split('/')[-1].split('.')[0]
+        f.write(' ')
+        f.write(os.path.join(mask_path, filename+'.png'))
+        f.write('\n')
